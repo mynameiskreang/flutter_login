783035a6ada9dcca78a9cdfa846913a3e21bdb9d
Kreangkrai Supchid <kreangkrai.sup@gmail.com>
2021-06-11 14:22:24 +0700

    add login_screen.dart

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_login/components/background.dart';
 import 'package:flutter_login/components/rounded_button.dart';
 import 'package:flutter_login/constants.dart';
+import 'package:flutter_login/screen/login/login_screen.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
@@ -9,31 +10,37 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Background(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Kreangkrai.com",
-            textScaleFactor: size.height * 0.003,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: size.height * 0.03),
-          SvgPicture.asset(
-            'assets/icons/chat.svg',
-            height: size.height * 0.45,
-          ),
-          SizedBox(height: size.height * 0.05),
-          RoundedButton(
-            text: 'Login',
-            press: () {},
-          ),
-          RoundedButton(
-            text: 'Sign Up',
-            color: kPrimaryLightColor,
-            textColor: Colors.black,
-            press: () {},
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Kreangkrai.com",
+              textScaleFactor: size.height * 0.003,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03),
+            SvgPicture.asset(
+              'assets/icons/chat.svg',
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: 'Login',
+              press: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return LoginScreen();
+                }));
+              },
+            ),
+            RoundedButton(
+              text: 'Sign Up',
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
